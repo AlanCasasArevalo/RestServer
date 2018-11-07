91d6d09332456a8ccecc1a4f8fe268193b758f30
Unknown <alancasasarevalo@gmail.com>
2018-11-07 20:58:49 +0100

    Google auth
    Google authorization was added to our project, html was added to test the google authentication

--- server/config/config.js
+++ server/config/config.js
@@ -37,3 +37,8 @@ process.env.TOKEN_END_VALIDATION = 60 * 60 * 24 * 30;
 //  SEED de autenticacion.
 // ********************//
 process.env.SEED = process.env.SEED || 'develop-secret-seed';
+
+//********************
+//  Google Client ID.
+// ********************//
+process.env.CLIENT_ID = process.env.CLIENT_ID || '676307275703-urekrt9tuovlte2vrbschm46s51815cr.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -3,20 +3,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(require('./routes/index'))
+app.use( express.static( path.resolve( __dirname + '../../public' )));
+console.log( path.resolve( __dirname + '../../public' ));
+
+app.use(require('./routes/index'));
+
 
 mongoose.connect(process.env.URLDDBB, (error, response) => {
-    if (error) throw error
+    if (error) throw error;
 
-    console.log('Base de datos OK')
-
+    console.log('Base de datos OK');
 });
 
 app.listen(process.env.PORT, function() {
-    console.log('Escuchando en el puerto:', process.env.PORT)
+    console.log('Escuchando en el puerto:', process.env.PORT);
 });
